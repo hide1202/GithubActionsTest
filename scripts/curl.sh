d7f9ab63d70d3fff7e3c5b490f1f467433b77597
Taegyeong Kim <hide1202@naver.com>
2022-04-08 22:09:46 +0900

    Create curl.sh

---
+++ scripts/curl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -f result.html
+URL='https://play.google.com/store/apps/details?id=com.movefastcompany.bora&gl=kr'
+curl "$URL" -o ./result.html
+STATUS_CODE=`curl -s -o /dev/null -w "%{http_code}\n" "$URL"`
+echo "$STATUS_CODE"
+if [ $STATUS_CODE = '200' ]
+then
+  echo 'success'
+  exit 0
+else
+  echo 'failure'
+  exit 1
+fi
